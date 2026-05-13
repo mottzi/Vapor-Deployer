88952db56bb5865fbf50cfc120efc4313da29203
mottzi <sayilir.berken@gmail.com>
2026-05-13 13:20:19 +0200

    feat: added remove binary button

--- Sources/Deployer/Panel/RowComponent.swift
+++ Sources/Deployer/Panel/RowComponent.swift
@@ -23,6 +23,7 @@ struct RowComponent: InstanceComponent {
             DeployAction(productName: productName),
             SaveBinaryAction(productName: productName),
             RestoreBinaryAction(productName: productName),
+            RemoveBinaryAction(productName: productName),
             DeleteAction(productName: productName),
             ToggleDetailsAction(productName: productName)
         ]
@@ -160,6 +161,43 @@ extension RowComponent {
             case .failure(let message):
                 .failure(message)
             }
+        }
+
+    }
+
+    struct RemoveBinaryAction: Action {
+
+        let name: String = "removeBinary"
+        let productName: String
+
+        func perform(targetID: UUID?, state: inout ComponentState, app: Application) async -> ActionResult {
+            
+            guard let targetID,
+                  let deployment = await loadDeployment(id: targetID, product: productName, app: app),
+                  deployment.hasSavedBinary
+            else { return .failure("Deployment not found or doesn't have a saved binary.") }
+            
+            let target = app.deployer.queue.config.target
+            let store = BinaryStore(target: target)
+            guard store.hasBinary(for: deployment)
+            else { return .failure("Saved binary not found on disk") }
+            
+            do {
+                try store.deleteBinary(for: deployment)
+                deployment.binarySizeMB = nil
+                deployment.isManuallySaved = false
+                try await deployment.save(on: app.db)
+            }
+            catch {
+                let mistError = MistError.databaseFetchFailed(
+                    "Binary delete id=\(deployment.id?.uuidString ?? "nil")",
+                    error
+                )
+                app.logger.error("\(mistError)")
+                return .failure("Failed to remove binary")
+            }
+            
+            return .success()
         }
 
     }
